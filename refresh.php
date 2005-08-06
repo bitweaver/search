867ee1aa4624350a89b6d0549aeac7b11be9e495
Lester Caine <lester@lsces.co.uk>
2005-08-06 18:31:30 +0000

    Tidy all database calls to remove ->mDb where possible

--- refresh.php
+++ refresh.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_search/refresh.php,v 1.1.1.1.2.1 2005/06/27 15:56:42 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_search/refresh.php,v 1.1.1.1.2.2 2005/08/06 18:31:30 lsces Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: refresh.php,v 1.1.1.1.2.1 2005/06/27 15:56:42 lsces Exp $
+ * $Id: refresh.php,v 1.1.1.1.2.2 2005/08/06 18:31:30 lsces Exp $
  * @author  Luis Argerich (lrargerich@yahoo.com)
  * @package search
  * @subpackage functions
@@ -73,6 +73,7 @@ function refresh_search_index() {
 
 		// hack around php database driver issues when a different database from bitweaver is accessed elsewhere during page  render
 		// this happens in the phpBB package when phpBB is in a different db from bitweaver in MySQL
+		// This only works on some databases
 		global $gBitSystem, $gBitDbName;
 		$gBitSystem->mDb->mDb->SelectDB( $gBitDbName );
 
