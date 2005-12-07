e5ec85323815db6ca353b0e396aacd16a5ad91fa
Stephan Borg <wolff_borg@users.sourceforge.net>
2005-12-07 06:24:20 +0000

    Fixed global var

--- modules/mod_global_search.php
+++ modules/mod_global_search.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_search/modules/mod_global_search.php,v 1.1.2.1 2005/10/30 10:09:04 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_search/modules/mod_global_search.php,v 1.1.2.2 2005/12/07 06:24:20 wolff_borg Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,12 +8,13 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: mod_global_search.php,v 1.1.2.1 2005/10/30 10:09:04 lsces Exp $
+ * $Id: mod_global_search.php,v 1.1.2.2 2005/12/07 06:24:20 wolff_borg Exp $
  * @author  Luis Argerich (lrargerich@yahoo.com)
  * @package search
  * @subpackage modules
  */
-vd($gLibertySystem->mContentTypes);
+global $gLibertySystem;
+//vd($gLibertySystem->mContentTypes);
 foreach( $gLibertySystem->mContentTypes as $cType ) {
 	$contentTypes[$cType['content_type_guid']] = $cType['content_description'];
 }
