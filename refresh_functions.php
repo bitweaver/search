256370f344e8d5210ea3a0419a1ce252fa4a1e2a
Max Kremmel <xing@synapse.plus.com>
2006-01-24 21:51:24 +0000

    merge recent changes from HEAD

--- refresh_functions.php
+++ refresh_functions.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_search/refresh_functions.php,v 1.1.1.1.2.9 2006/01/24 21:36:21 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_search/refresh_functions.php,v 1.1.1.1.2.10 2006/01/24 21:51:24 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: refresh_functions.php,v 1.1.1.1.2.9 2006/01/24 21:36:21 lsces Exp $
+ * $Id: refresh_functions.php,v 1.1.1.1.2.10 2006/01/24 21:51:24 squareing Exp $
  * @author  Luis Argerich (lrargerich@yahoo.com)
  * @package search
  * @subpackage functions
@@ -123,7 +123,7 @@ function random_refresh_index_articles() {
 					WHERE ta.`content_id`=tc.`content_id` AND uu.`user_id` = tc.`user_id`";
 			$result=$gBitSystem->mDb->query($query,array(),1,rand(0,$cant-1));
 			$res=$result->fetchRow();
-			$words=search_index($res["title"]." ".$res["author_name"]." ".$res["heading"]." ".$res["body"]." ".$res["author"]);
+			$words=search_index($res["title"]." ".$res["author_name"]." ".$res["heading"]." ".$res["data"]." ".$res["author"]);
 			insert_index($words,BITARTICLE_CONTENT_TYPE_GUID,$res["article_id"]);
 		}
 	}
