31ab777c9355ec8161b5539a31b5ab97432e7084
Max Kremmel <xing@synapse.plus.com>
2006-02-01 21:07:39 +0000

    use correct feature verification

--- directory_search.php
+++ directory_search.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_search/directory_search.php,v 1.1.1.1.2.2 2005/07/26 15:50:26 drewslater Exp $
+ * $Header: /cvsroot/bitweaver/_bit_search/directory_search.php,v 1.1.1.1.2.3 2006/02/01 21:07:39 squareing Exp $
  *
  * Copyright (c) 2004 bitweaver.org
  * Copyright (c) 2003 tikwiki.org
@@ -8,7 +8,7 @@
  * All Rights Reserved. See copyright.txt for details and a complete list of authors.
  * Licensed under the GNU LESSER GENERAL PUBLIC LICENSE. See license.txt for details
  *
- * $Id: directory_search.php,v 1.1.1.1.2.2 2005/07/26 15:50:26 drewslater Exp $
+ * $Id: directory_search.php,v 1.1.1.1.2.3 2006/02/01 21:07:39 squareing Exp $
  * @author  Luis Argerich (lrargerich@yahoo.com)
  * @package search
  * @subpackage functions
@@ -21,19 +21,8 @@ require_once( '../bit_setup_inc.php' );
 
 include_once( DIRECTORY_PKG_PATH.'dir_lib.php' );
 
-if ($feature_directory != 'y') {
-	$gBitSmarty->assign('msg', tra("This feature is disabled").": feature_directory");
-
-	$gBitSystem->display( 'error.tpl' );
-	die;
-}
-
-if (!$gBitUser->hasPermission( 'bit_p_view_directory' )) {
-	$gBitSmarty->assign('msg', tra("Permission denied"));
-
-	$gBitSystem->display( 'error.tpl' );
-	die;
-}
+$gBitSystem->verifyFeature( 'feature_directory' );
+$gBitSystem->verifyPermission( 'bit_p_view_directory' );
 
 $gBitSmarty->assign('words', $_REQUEST['words']);
 $gBitSmarty->assign('where', $_REQUEST['where']);
