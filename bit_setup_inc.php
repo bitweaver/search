fa2947557d17bd89c472c1e294b832c6ef8548e6
Sean Lee <seannerd@users.sourceforge.net>
2006-02-09 02:56:09 +0000

    Removed random index refresing - obsolete with search as a service.

--- bit_setup_inc.php
+++ bit_setup_inc.php
@@ -6,13 +6,6 @@ $gBitSystem->registerPackage( 'search', dirname( __FILE__).'/', TRUE, LIBERTY_SE
 if( $gBitSystem->isPackageActive( 'search' ) ) {
 	$gBitSystem->registerAppMenu( SEARCH_PKG_NAME, ucfirst( SEARCH_PKG_DIR ), SEARCH_PKG_URL.'index.php', '', 'search');
 
-	// **********  SEARCH  ************
-	// Register the search refresh function
-	// But only if the Index On Submit isn't set
-	if( ! $gBitSystem->isFeatureActive("search_index_on_submit") ) {
-		include_once( SEARCH_PKG_PATH.'refresh.php' );
-		register_shutdown_function("refresh_search_index");
-	}
 	include_once( SEARCH_PKG_PATH.'refresh_functions.php' );
 	$gLibertySystem->registerService( LIBERTY_SERVICE_SEARCH, SEARCH_PKG_NAME, 
 		array('content_store_function' => 'refresh_index'));
